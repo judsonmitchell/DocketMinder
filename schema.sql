2374b9470efd70a634f2b2c2e5c2c300f435d4c9
Judson Mitchell <judsonmitchell@gmail.com>
2013-03-29 08:32:49 -0500

    Update schema

--- schema.sql
+++ schema.sql
@@ -1,32 +1,8 @@
--- phpMyAdmin SQL Dump
--- version 3.4.10.1deb1
--- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Mar 27, 2013 at 05:53 AM
--- Server version: 5.5.28
--- PHP Version: 5.3.10-1ubuntu3.5
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- Database: `test`
+-- Table structure for table `docketminder_cases`
 --
 
--- --------------------------------------------------------
-
---
--- Table structure for table `cases`
---
-
-CREATE TABLE IF NOT EXISTS `cases` (
+CREATE TABLE IF NOT EXISTS `docketminder_cases` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(200) NOT NULL,
   `number` varchar(50) NOT NULL,
@@ -36,22 +12,20 @@ CREATE TABLE IF NOT EXISTS `cases` (
   `last_changed` datetime NOT NULL,
   `last_tracked` datetime NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=44 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=47 ;
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+-- Table structure for table `docketminder_users`
 --
 
-CREATE TABLE IF NOT EXISTS `users` (
+CREATE TABLE IF NOT EXISTS `docketminder_users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(500) NOT NULL,
   `username` varchar(200) NOT NULL,
   `password` varchar(200) NOT NULL,
   `email` varchar(200) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=10 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=11 ;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
